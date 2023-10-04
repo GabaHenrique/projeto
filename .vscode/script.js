6d3425ff087cef41e49c7a29c3ba089e2950cb7d
Gaba <gaaba.henrique2@gmail.com>
2023-10-04 18:26:37 -0300

    change background based on media queries

---
+++ .vscode/script.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+const html = document.documentElement
+html.classList.toggle('light')
+
+// ao inves de colocar tudo isso, posso apenas colocar ''toggle''
+
+//pegar a tag 'img'
+const img = document.querySelector('#profile img')
+
+//substituir a imagem 
+
+if (html.classList.contains('light')) {
+//se tivr light mode adicionar img light 
+
+img.setAttribute('src', './assets/avatar-light.png')
+//setiver sem ligh mode, usar avatar padrão
+}
+else {
+img.setAttribute('src','./assets/avatar.png')
+
+}
+}
+
+
+
